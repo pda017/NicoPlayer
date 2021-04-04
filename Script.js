6a1a759fe37d8f0f60e31c9b3be7bd561fc2804c
pda017 <pda017a@gmail.com>
2021-04-04 13:32:43 +0900

    중첩개선

--- Script.js
+++ Script.js
@@ -47,6 +47,7 @@ var textAlpha = scriptEle.getAttribute("textAlpha");
 var mentSpeed = 0.25;
 var useFont ="Nanum Gothic";
 var g_MentList;
+var CheckMentBoxColInterval = 0;
 
 if(isYoutube)
 {
@@ -62,8 +63,9 @@ fetch(mentUrl).then(res=>res.text())
 .then(data=>
     {
         g_MentList = ReadNicoComments(data,fontSize);
-        UpdateMentY(g_MentList,canvas);
+        UpdateMentYAll(g_MentList,canvas);
         setInterval(()=>UpdateText());
+        CheckMentBoxColInterval = setInterval(()=>CheckMentBoxCol());
         loadingButton.style.display = "none";
         /*
         for(var ml of mentList)
@@ -75,6 +77,76 @@ fetch(mentUrl).then(res=>res.text())
     .catch(err=>console.log(err));
 
 //function
+function CheckBoxCol(x1,y1,w1,h1,x2,y2,w2,h2)
+{
+    var maxX1 = (x1+w1);
+    var maxY1 = (y1+h1);
+    var maxX2 = (x2+w2);
+    var maxY2 = (y2+h2);
+    if (maxX1 < x2 || x1 > maxX2)
+    {
+        return false;
+    }
+    if (maxY1 < y2 || y1> maxY2) 
+    {
+        return false;
+    }
+    return true;
+}
+function FixColMentY(ment,mentX,duration,fullLength)
+{
+    for(var colMent of g_MentList)
+    {
+        if(colMent.timeline == ment.timeline)
+        {
+            continue;
+        }
+        var colX = (colMent.timeline / duration) * fullLength;
+        if(CheckBoxCol(mentX,ment.posY,ment.width,ment.height
+            ,colX,colMent.posY,colMent.width,colMent.height))
+        {
+            //console.log(ment.comment + "(Col)");
+            UpdateMentY(ment,canvas);
+        }
+    }
+}
+
+function CheckMentBoxCol()
+{
+    var duration = 0;
+    if(isYoutube)
+    {
+        if(!IsYoutubeLoaded())
+        {
+            return;
+        }
+        duration = ytPlayer.getDuration();
+    }
+    else
+    {
+        duration = video.duration;
+    }
+
+    var fullLength = duration * canvas.width * mentSpeed;
+    for(var ment of g_MentList)
+    {
+        var mentX = (ment.timeline / duration) * fullLength;
+        FixColMentY(ment,mentX,duration,fullLength);
+    }
+
+    clearInterval(CheckMentBoxColInterval);
+    console.log("clearInterval(CheckMentBoxColInterval)");
+}
+
+function IsYoutubeLoaded()
+{
+    if(!ytPlayer || ytPlayer.getDuration == null || ytPlayer.getCurrentTime == null)
+    {
+        return false;
+    }
+    return true;
+}
+
 function UpdateText()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -82,7 +154,7 @@ function UpdateText()
     var currentTime;
     if(isYoutube)
     {
-        if(!ytPlayer || ytPlayer.getDuration == null || ytPlayer.getCurrentTime == null)
+        if(!IsYoutubeLoaded())
         {
             return;
         }
@@ -97,11 +169,12 @@ function UpdateText()
     var fullLength = duration * canvas.width * mentSpeed;
     for(var ment of g_MentList)
     {
-        var oriMentX = ((ment.timeline / duration) * fullLength) + canvas.width;
-        var videoX = ((currentTime / duration) * fullLength);
-        var mentX = oriMentX - videoX;
+        //var oriMentX = ((ment.timeline / duration) * fullLength) + canvas.width;
+        //var videoX = ((currentTime / duration) * fullLength);
+        var mentX = (((ment.timeline - currentTime) / duration) * fullLength) + canvas.width;
         if(mentX + ment.width < 0 || mentX > canvas.width)
             continue;
+        //ctx.fillRect(mentX,ment.posY,ment.width,ment.height);
         DrawText(mentX,ment.posY,ment.comment,ment.size,ment.pos,ment.color);
     }
 }
@@ -173,26 +246,29 @@ function launchIntoFullscreen(element) {
     }
  }
 
-function UpdateMentY(mentList,canvasEle)
+function UpdateMentYAll(mentList,canvasEle)
 {
     for(var ment of mentList)
     {
-        if(ment.pos == 0) //for regular moving comment,
-        {
-            var randY = rand(0,canvasEle.height-ment.size);
-            ment.posY = randY;
-        }
-        else if(ment.pos == 1) //bottom
-        {
-            var randY = rand((canvasEle.height/2) + ment.size,canvasEle.height-ment.size);
-            ment.posY = randY;
-        }
-        else if(ment.pos == 2) //top
-        {
-            var randY = rand(ment.size,(canvasEle.height/2) - ment.size);
-            ment.posY = randY;
-        }
-        //console.log(ment.timeline);
+        UpdateMentY(ment,canvasEle);
+    }
+}
+function UpdateMentY(ment,canvasEle)
+{
+    if(ment.pos == 0) //for regular moving comment,
+    {
+        var randY = rand(0,canvasEle.height-ment.size);
+        ment.posY = randY;
+    }
+    else if(ment.pos == 1) //bottom
+    {
+        var randY = rand((canvasEle.height/2) + ment.size,canvasEle.height-ment.size);
+        ment.posY = randY;
+    }
+    else if(ment.pos == 2) //top
+    {
+        var randY = rand(ment.size,(canvasEle.height/2) - ment.size);
+        ment.posY = randY;
     }
 }
 function ReadNicoComments(xmlFile,fsize)
